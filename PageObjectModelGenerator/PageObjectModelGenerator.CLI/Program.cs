77dbd0422ecb277cee1ce1828b1ca349853d5375
M.Suska <msuska@gmail.com>
2018-03-21 22:05:14 +0100

    Creating empty solution

---
+++ PageObjectModelGenerator/PageObjectModelGenerator.CLI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PageObjectModelGenerator.CLI
+{
+    class Program
+    {
+        /*
+         * 1. to do - get window title from process
+         * 2. get all descendants recirsively
+         */
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
